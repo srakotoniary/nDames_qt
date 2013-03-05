66f70aa0b55b3106de5ca64dc0450b2656583829
Elkami_kaze <etudiant@LF9.localdomain>
2013-03-05 09:46:33 +0100

    ajout d'attribut et constructeur pour Algo.h

--- algo.cpp
+++ algo.cpp
@@ -1,5 +1,59 @@
 #include "algo.h"
+#include <iostream>
 
-Algo::Algo()
+Algo::Algo(int nb): nbReine(nb)
 {
+    QVector<int> ligne;
+
+    for(int i = 0; i < nb; i++){
+        for(int j = 0; j < nb; j++)
+            ligne.push_back(0);
+        Echiquier.push_back(ligne);
+        ligne.clear();
+    }
+
+    for(int i = 0; i < nb; i++){
+        for(int j = 0; j < nb; j++)
+            std::cout << Echiquier[i][j];
+       std::cout << std::endl;
+    }
+}
+
+int Algo::generatetest(int nbReines){
+/*
+    QPair<int,QPoint> Generates;
+    QVector<QPoint> Generate;
+    QVector<QPoint> Solution;
+    QPoint reine;
+    // int ligne = nbReines;
+    // int colonne = nbReines;
+
+    //Boucle
+    for(int ri = 0; ri<nbReines; ri++){
+        for(int rj = 0; rj<nbReines;rj++){
+
+            //initialise la position de la reine "fixe"
+            reine.setX(ri);
+            reine.setY(rj);
+            Generate.push_back(reine);
+
+            //Parcours des différents solution pour une reine "fixe" à (ri,rj)
+            for(int i = 0; i<nbReines; i++){
+                for(int j = 0; j<nbReines; j++){
+                    //Initialise la position de la reine suivante placé en (i,j), si la position est différente de la reine "fixe"
+                    if(!(i==ri && j==rj)){
+                        reine.setX(i);
+                        reine.setY(j);
+                        Generate.push_back(reine);
+                    }
+                }
+                //fin
+
+            }
+
+            //Un cas
+        }
+    }
+*/
+    return 0;
 }
--- algo.h
+++ algo.h
@@ -1,10 +1,17 @@
 #ifndef ALGO_H
 #define ALGO_H
+#include <QtGui>
 
 class Algo
 {
+private:
+    QVector<QVector<int> > Echiquier;
+    int nbReine;
+
 public:
-    Algo();
+    Algo(int nb);
+    int generatetest(int nbReines);
+   // QPoint placeReine(int i, int j, Qvector<QPoint> reines); //teste et place (si c'est possible) une reine a la case (i,j)
 };
 
 #endif // ALGO_H
