a1a7de271f00bdad555afefcd51f84efb66194a7
srakotoniary <sombi.rakotoniary@etud.univ-angers.fr>
2013-03-29 18:34:43 +0100

    reine joli

--- case.cpp
+++ case.cpp
@@ -17,11 +17,11 @@ Case::Case(int x, int y, bool coul): QLabel()
     dameSurLaCase=false;
     couleur =coul;
     setMinimumSize(50,50);
-    if (couleur==true) {setStyleSheet("QLabel { background-color : red; color : white; }");}
-    else {setStyleSheet("QLabel { background-color : blue; color : white; }");}
+    if (couleur==true) {setStyleSheet("QLabel { background-color : white; color : white; }");}
+    else {setStyleSheet("QLabel { background-color : black; color : white; }");}
 
 /*
-    if (couleur==true) {setText("Blanc"); setStyleSheet("QLabel { background-color : red; color : white; }");}
+    if (couleur==true) {setText("Blanc"); setStyleSheet("QLabel { background-color : green; color : white; }");}
     else {setText("Noir"); setStyleSheet("QLabel { background-color : blue; color : white; }");}
     std::cout<<"coordonnees de la case: "<<position.x()<<","<<position.y()<<std::endl;
 */
@@ -47,7 +47,7 @@ void Case::setDameSurLaCase(bool val)
     dameSurLaCase=val;
     //QString CurrentDir = QDir::currentPath();
     //std::cout<<CurrentDir.toStdString()<<std::endl;
-    QPixmap pixmap("image/queen3.svg");
+    QPixmap pixmap("image/quee.svg");
     //std::cout<<"largeur" <<pixmap.isNull()<<std::endl;
 
     setPixmap(pixmap.scaledToWidth(this->width()));
