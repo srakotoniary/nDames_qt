00de4a09b4db93104f908bd3cd3ad981eac70538
Elkami_kaze <etudiant@LF9.localdomain>
2013-03-30 09:44:05 +0100

    ajout classe recherchelocal

---
+++ recherchelocal.cpp
@@ -0,0 +1,12 @@
+#include "recherchelocal.h"
+
+RechercheLocal::RechercheLocal(int nb): Algo(nb,0)
+{
+    execution(0);
+//    std::cout << "nombre de Sol" << nbSol.size()<<std::endl;
+//     std::cout << "nombre Iter" << nbIter <<std::endl;
+}
+
+void RechercheLocal::execution(int x){
+
+}
---
+++ recherchelocal.h
@@ -0,0 +1,12 @@
+#ifndef RECHERCHELOCAL_H
+#define RECHERCHELOCAL_H
+#include "algo.h"
+
+class RechercheLocal : public Algo
+{
+public:
+    RechercheLocal(int nb);
+    void execution(int x);
+};
+
+#endif // RECHERCHELOCAL_H
